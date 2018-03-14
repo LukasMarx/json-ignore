42b620d1bbec26ad5ff63c4334ffa929c1860fa6
Lukas Marx <marxlukas@googlemail.com>
2018-03-14 09:57:25 +0100

    added d.ts file to npm package

--- index.ts
+++ index.ts
@@ -1,46 +1,45 @@
-import "reflect-metadata";
+import 'reflect-metadata';
 
-const formatMetadataKey = "jsonIgnore";
-const jsonReplaceByKeyConst="jsonReplaceByKey"
-const jsonReplaceByValueConst="jsonReplaceByValue"
+const formatMetadataKey = 'jsonIgnore';
+const jsonReplaceByKeyConst = 'jsonReplaceByKey';
+const jsonReplaceByValueConst = 'jsonReplaceByValue';
 
-export function jsonIgnore(){
-    return Reflect.metadata(formatMetadataKey,true);
+export function jsonIgnore() {
+    return Reflect.metadata(formatMetadataKey, true);
 }
 
-export  function getJsonIgnore(object:any, propertyKey:string):boolean{
-    return Reflect.getMetadata(formatMetadataKey,object,propertyKey);
+export function getJsonIgnore(object: any, propertyKey: string): boolean {
+    return Reflect.getMetadata(formatMetadataKey, object, propertyKey);
 }
 
-export  function jsonIgnoreReplacer(key:string, value:any){
-    let meta = getJsonIgnore(this,key);
-    if(meta===true){
+export function jsonIgnoreReplacer(key: string, value: any) {
+    let meta = getJsonIgnore(this, key);
+    if (meta === true) {
         return undefined;
     }
-    let constant= getJsonReplaceByConstant(this,key);
-    if(constant!=null){
+    let constant = getJsonReplaceByConstant(this, key);
+    if (constant != null) {
         return constant;
     }
-    let childKey= getJsonReplaceByChildValue(this,key);
-    if(childKey!=null){
+    let childKey = getJsonReplaceByChildValue(this, key);
+    if (childKey != null) {
         return value[childKey];
-        
     }
     return value;
 }
 
-export function jsonReplaceByChildValue(childKey:string){
-     return Reflect.metadata(jsonReplaceByKeyConst, childKey);
+export function jsonReplaceByChildValue(childKey: string) {
+    return Reflect.metadata(jsonReplaceByKeyConst, childKey);
 }
 
-export function jsonReplaceByConstant(value:any){
-     return Reflect.metadata(jsonReplaceByValueConst,value);
+export function jsonReplaceByConstant(value: any) {
+    return Reflect.metadata(jsonReplaceByValueConst, value);
 }
 
-export  function getJsonReplaceByChildValue(object:any, propertyKey:string):string{
-    return Reflect.getMetadata(jsonReplaceByKeyConst,object,propertyKey);
+export function getJsonReplaceByChildValue(object: any, propertyKey: string): string {
+    return Reflect.getMetadata(jsonReplaceByKeyConst, object, propertyKey);
 }
 
-export  function getJsonReplaceByConstant(object:any, propertyKey:string):any{
-    return Reflect.getMetadata(jsonReplaceByValueConst,object,propertyKey);
+export function getJsonReplaceByConstant(object: any, propertyKey: string): any {
+    return Reflect.getMetadata(jsonReplaceByValueConst, object, propertyKey);
 }
